b076ce12f9aed35fdca990f59ad35f307dc4bd07
hnahdi <hnahdi@ats-digital.com>
2022-12-15 13:15:27 +0100

    some optimizations

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,108 +1,118 @@
 import "./App.css";
 import Todo from "./components/todos/Todo.component";
-import LoginForm from "./components/login/Login.component"
+import LoginForm from "./components/login/Login.component";
 import {
-    createBrowserRouter,
-    RouterProvider, useNavigate, useParams,
+  createBrowserRouter,
+  RouterProvider,
+  useNavigate,
+  useParams,
 } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar.component";
 import Form from "./components/form/form.component";
-import {Task} from "./model/Task";
+import { Task } from "./model/Task";
 import React from "react";
 import PrivateRoute from "./utils/PrivateRoute";
 import axios from "axios";
 import navbarComponent from "./components/navbar/Navbar.component";
 
 const LoginElementForRoutes = () => {
-
-  return(
-      <>
-        <Navbar />
-        <LoginForm onSubmit={(username, password)=> localStorage.setItem('username',JSON.stringify({
-        username,password
-        }))}/>
-      </>
-  )
-}
+  return (
+    <>
+      <Navbar />
+      <LoginForm
+        onSubmit={(username, password) =>
+          localStorage.setItem(
+            "username",
+            JSON.stringify({
+              username,
+              password,
+            })
+          )
+        }
+      />
+    </>
+  );
+};
 
 const ListElementForRoutes = () => {
-    return(
-        // @ts-ignore
-        <PrivateRoute>
-            <Navbar />
-            <Todo />
-        </PrivateRoute>
-    )
-}
+  return (
+    // @ts-ignore
+    <PrivateRoute>
+      <Navbar />
+      <Todo />
+    </PrivateRoute>
+  );
+};
 
 const AddElementForRoutes = () => {
-
-    const navigate = useNavigate();
-    return (
-      <PrivateRoute>
-        <Navbar />
-        <Form
-            // @ts-ignore
-          submit={async (task: Task) => {
-              try {
-                  const {data} = await axios.post('http://localhost:8000/todo',task);
-                  if(data){
-                      console.log(data);
-                      navigate('/list');
-                  }
-              }catch (e) {
-                  console.log(e);
-              }
-          }}
-        />
-      </PrivateRoute>
-    );
-}
+  const navigate = useNavigate();
+  return (
+    <PrivateRoute>
+      <Navbar />
+      <Form
+        // @ts-ignore
+        submit={async (task: Task) => {
+          try {
+            const { data } = await axios.post(
+              "http://localhost:8000/todo",
+              task
+            );
+            if (data) {
+              console.log(data);
+              navigate("/list");
+            }
+          } catch (e) {
+            console.log(e);
+          }
+        }}
+      />
+    </PrivateRoute>
+  );
+};
 
 const UpdateElementForRoutes = () => {
-    const {id} = useParams();
-    const navigate = useNavigate();
-    return(
-        <PrivateRoute>
-            <Navbar />
-            <Form submit={async (todo?: Task)=>{
-                try {
-                    const {data} = await axios.patch(`http://localhost:8000/todo/${id}`,todo);
-                    if(data){
-                        navigate('/list');
-                    }
-                }catch (e) {
-                    console.log(e);
-                }
+  const { id } = useParams();
+  const navigate = useNavigate();
+  return (
+    <PrivateRoute>
+      <Navbar />
+      <Form
+        submit={async (todo?: Task) => {
+          try {
+            const { data } = await axios.patch(
+              `http://localhost:8000/todo/${id}`,
+              todo
+            );
+            if (data) {
+              navigate("/list");
             }
-            }/>
-        </PrivateRoute>
-    )
-}
-
-const router = createBrowserRouter([
+          } catch (e) {
+            console.log(e);
+          }
+        }}
+      />
+    </PrivateRoute>
+  );
+};
+export const routes = [
   {
     path: "",
-    element: <LoginElementForRoutes/>,
+    element: <LoginElementForRoutes />,
   },
   {
-    path: "list",
+    path: "/list",
     element: <ListElementForRoutes />,
   },
   {
-    path: "add",
+    path: "/add",
     element: <AddElementForRoutes />,
   },
   {
     path: "/update/:id",
     element: <UpdateElementForRoutes />,
-  }
-]);
-const App = () => (
-      <RouterProvider router={router} />
-);
-
-
-
+  },
+];
+const router = createBrowserRouter(routes);
+const App = () => <RouterProvider router={router} />;
 
 export default App;
--- front/src/components/navbar/Navbar.component.tsx
+++ front/src/components/navbar/Navbar.component.tsx
@@ -1,47 +1,56 @@
-import React from 'react';
-import './Navbar.style.css';
-import {Link, useNavigate} from "react-router-dom";
+import React from "react";
+import "./Navbar.style.css";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
+import { routes } from "../../App";
 
 const Navbar: React.FC = () => {
-    // @ts-ignore
-    let username  = useAuth()
-     const navigate = useNavigate();
-    const renderTab:Object = {
-     logout_Login: username ? <Link to={'/'} onClick={()=>{
-         localStorage.removeItem('username')
-        navigate('/');
-     }
-     }>Logout</Link>:<Link to={'/'}>Login</Link>,
-     username:username && <Link to={'/list'}>{username}</Link>,
-     add:username && <Link to={'/add'}>add</Link>,
-     list:username && <Link to={'/list'}>List</Link>,
-    }
+  // @ts-ignore
+  let username = useAuth();
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const renderTab: Object = {
+    logout_Login: username ? (
+      <Link
+        to={"/"}
+        onClick={() => {
+          localStorage.removeItem("username");
+          navigate("/");
+        }}
+      >
+        Logout
+      </Link>
+    ) : (
+      <Link to={"/"}>Login</Link>
+    ),
+    username: username && <Link to={"/list"}>{username}</Link>,
+    add: username && <Link to={"/add"}>add</Link>,
+    list: username && <Link to={"/list"}>List</Link>,
+  };
 
-    return(
-        <nav>
-            <div className="logo">My App</div>
-            <div>
-                {
-                    // @ts-ignore
-                    renderTab['username']
-                }
-                {
-                    // @ts-ignore
-                     renderTab['logout_Login']
-                }
-                {
-                    // @ts-ignore
-                    renderTab['list']
-                }
-                {
-                    // @ts-ignore
-                    renderTab['add']
-                }
-            </div>
-        </nav>
-        )
-
+  return (
+    <nav>
+      <div className="logo">My App</div>
+      <div>
+        {
+          // @ts-ignore
+          renderTab["username"]
+        }
+        {
+          // @ts-ignore
+          renderTab["logout_Login"]
+        }
+        {
+          // @ts-ignore
+          renderTab["list"]
+        }
+        {
+          // @ts-ignore
+          renderTab["add"]
+        }
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
